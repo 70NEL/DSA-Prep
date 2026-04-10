eaf73fa9d9f53406f7ea03c18d3080d7377b31fb
Tony Ardelean <toniardelean@yahoo.com>
2026-04-10 15:41:42 +0300

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.java
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -5,7 +5,7 @@ class Solution {
             if(map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target-nums[i]), i};
             }else {
-                map.put(nums[i],i);
+                map.put(Integer.valueOf(nums[i]), Integer.valueOf(i));
             }
         }
         return null;
